b4eb368171feffa32f8cb851cb2c0673628cc0b4
Jesus Perales <jesus.perales@kaf.com.mx>
2014-01-29 01:09:21 -0600

    Programa basico para sumar dos numeros en android

---
+++ src/com/moviles/sumadosnumeros/Operacion.java
@@ -0,0 +1,9 @@
+package com.moviles.sumadosnumeros;
+
+public class Operacion {
+	
+	public Double sumar(Double numero1, Double numero2){
+		return (numero1 + numero2);
+	}
+
+}
---
+++ src/com/moviles/sumadosnumeros/Suma.java
@@ -0,0 +1,65 @@
+package com.moviles.sumadosnumeros;
+
+import java.io.Console;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Suma extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_suma);
+		//
+		Button ejecutarSuma = (Button)findViewById(R.id.ejecutarSuma);
+		ejecutarSuma.setOnClickListener(sumar);
+	}
+	
+	public void operacionSuma(){
+		Operacion suma = new Operacion();
+		EditText primerNumText = (EditText)findViewById(R.id.numeroUno);
+		EditText segundoNumText = (EditText)findViewById(R.id.numeroDos);
+		TextView resultado = (TextView)findViewById(R.id.resultadoSuma);
+		//debug
+		String test = primerNumText.toString();
+		System.out.print(primerNumText.toString());
+		//
+		Double primerNumero = Double.parseDouble(primerNumText.getText().toString());
+		Double segundoNumero = Double.parseDouble(segundoNumText.getText().toString());
+		Double resultadoSuma = suma.sumar(primerNumero, segundoNumero);
+		System.out.print(resultadoSuma);
+		resultado.setText(resultadoSuma.toString());
+	}
+	
+	/*
+	public void operacionSuma(){
+		Operacion suma = new Operacion();
+		EditText primerNumText = (EditText)findViewById(R.id);
+		EditText segundoNumText = (EditText)findViewById(R.id.segundoNumero);
+		EditText resultadoText = (EditText)findViewById(R.id.resultado);
+		double primerNumero = Double.parseDouble(primerNumText.toString());
+		double segundoNumero = Double.parseDouble(segundoNumText.toString());
+		double resultado = suma.sumar(primerNumero, segundoNumero);
+	}
+	*/
+	private OnClickListener sumar = new OnClickListener() {		
+		@Override
+		public void onClick(View view) {
+			operacionSuma();
+		}
+	};
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.suma, menu);
+		return true;
+	}
+}
